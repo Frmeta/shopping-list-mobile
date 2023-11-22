14c33f66a43e463e8d5a62d5772c41416c957994
Fredo Melvern Tanzil <fredotanzil@gmail.com>
2023-11-22 14:06:18 +0700

    tes

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,6 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
           useMaterial3: true,
         ),
-        
         //home: MyHomePage(),
         home: const LoginPage(),
       ),
